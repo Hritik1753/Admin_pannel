96a14745943b07b9c32761243573ef7d04b2c2d7
Hritik1753 <157448773+Hritik1753@users.noreply.github.com>
2024-01-28 09:34:06 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-
-import React from "react";
-
-
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import List from "./pages/list/List";
-import New from "./pages/new/New";
 import Single from "./pages/single/Single";
+import New from "./pages/new/New";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { productInputs, userInputs } from "./formSource";
+import "./style/dark.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
 
 function App() {
+  const { darkMode } = useContext(DarkModeContext);
+
   return (
-    <div className="App">
-         
+    <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
-        
-      <Routes>
+        <Routes>
           <Route path="/">
             <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
@@ -24,25 +24,20 @@ function App() {
               <Route path=":userId" element={<Single />} />
               <Route
                 path="new"
-                element={<New/>}
+                element={<New inputs={userInputs} title="Add New User" />}
               />
-               </Route>
+            </Route>
             <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
               <Route
                 path="new"
-                element={<New/>}
+                element={<New inputs={productInputs} title="Add New Product" />}
               />
             </Route>
-
-            </Route>
-
+          </Route>
         </Routes>
-          
       </BrowserRouter>
-   
-
     </div>
   );
 }
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,17 +1,18 @@
-import React from "react";
-import "./list.scss";
+import "./list.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Datatable from "../../components/datatable/Datatable"
 
 const List = () => {
-    return (
-        <>
-            <div className="list">
-                <div className="title">
-                    <h1>i am list of admin</h1>
-                </div>
-                
-                
-            </div>
-        </>
-    )
+  return (
+    <div className="list">
+      <Sidebar/>
+      <div className="listContainer">
+        <Navbar/>
+        <Datatable/>
+      </div>
+    </div>
+  )
 }
+
 export default List
